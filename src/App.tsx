b638a196bf7c1bcbca81eef1581c9cfe7bf13086
Andre Dionisio <andredionisio12@gmail.com>
2021-04-02 19:31:46 -0300

    defining global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
 
+import GlobalStyle from './styles/global';
+
 const App: React.FC = () => {
-  return <h1>Hello GoBarber</h1>;
+  return (
+    <>
+      <GlobalStyle />
+    </>
+  );
 };
 
 export default App;
